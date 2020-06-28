a6e8b96036f2f5c21b01c21a57feaaf28d41e838
sarab <sarabjot294@gmail.com>
2020-06-28 16:44:33 +0530

    The Nine Hertz Completed

--- app/src/main/java/com/example/the_nine_hertz/activities/MainActivity.java
+++ app/src/main/java/com/example/the_nine_hertz/activities/MainActivity.java
@@ -201,6 +201,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onBackPressed() {
+        audioRecord.stopPlaying();
         finish();
         super.onBackPressed();
     }
